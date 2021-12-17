b6d820673841e0c499987ebb4bb04f36d9aa1d9a
钢蛋 <luoyungui@tuhu.cn>
2021-12-17 22:31:38 +0800

    init

--- .umirc.local.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  define: {
-    'process.env.BASE_URL': 'http://localhost:3000',
-  },
-};
--- .umirc.prod.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  define: {
-    'process.env.BASE_URL': 'http://81.68.101.109:3000',
-  },
-};
